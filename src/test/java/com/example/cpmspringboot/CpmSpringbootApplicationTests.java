aa5d88ba1437707474b2f4158ce17358ac8abfaa
saglogog <saglogog@gmail.com>
2023-06-22 11:52:28 +0300

    first commit cpm springboot

---
+++ src/test/java/com/example/cpmspringboot/CpmSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cpmspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpmSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
